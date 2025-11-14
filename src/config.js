b55f59f32e27a7cd4a6c9a6ab19a445d96abe3f4
Sahil Ganbhoj <ganbhojsahil@gmail.com>
2025-11-14 15:49:22 +0530

    Changes env

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+export const API_URL = process.env.REACT_APP_API_URL || "http://tata-pwa-frontend.vercel.app";
 export const APP_NAME = "Talent Tracker";
 export const DEFAULT_DATE_FORMAT = "YYYY-MM-DD";
